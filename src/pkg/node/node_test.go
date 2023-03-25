2d9b873d229c4de98b8156992b96bf5876141483
horvatic <sbhorvatic@gmail.com>
2023-03-25 11:35:16 -0500

    add system info

--- src/pkg/node/node_test.go
+++ src/pkg/node/node_test.go
@@ -1,21 +1,21 @@
 package node
 
-import ( 
-	"testing"
+import (
 	"reflect"
+	"testing"
 )
 
 func TestGets(t *testing.T) {
 	sampleTags := []string{"arm", "x64"}
 	sampleNode := NewNode("1.1.1.0", "test", sampleTags)
 
-    if sampleNode.GetIp() != "1.1.1.0" {
-        t.Errorf("GetIp() = %v; 1.1.1.0", sampleNode.GetIp())
-    }
+	if sampleNode.GetIp() != "1.1.1.0" {
+		t.Errorf("GetIp() = %v; 1.1.1.0", sampleNode.GetIp())
+	}
 	if sampleNode.GetName() != "test" {
-        t.Errorf("GetName() = %v; test", sampleNode.GetName())
-    }
+		t.Errorf("GetName() = %v; test", sampleNode.GetName())
+	}
 	if !reflect.DeepEqual(sampleNode.GetTags(), []string{"arm", "x64"}) {
-        t.Errorf("GetTags() = %v; [arm x64]", sampleNode.GetTags())
-    }
+		t.Errorf("GetTags() = %v; [arm x64]", sampleNode.GetTags())
+	}
 }
