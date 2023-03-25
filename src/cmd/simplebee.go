8eaa947e571f1b83b95f0b09872fbe28bebc50f4
horvatic <sbhorvatic@gmail.com>
2023-03-25 10:51:30 -0500

    add testing base

--- src/cmd/simplebee.go
+++ src/cmd/simplebee.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/horvatic/simple-bee/pkg/node"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	sampleTags := []string{"arm", "x64"}
+	sampleNode := node.NewNode("1.1.1.0", "test", sampleTags)
+
+	fmt.Println(sampleNode.GetIp())
+	fmt.Println(sampleNode.GetName())
+	fmt.Println(sampleNode.GetTags())
 }
---
+++ src/pkg/node/node.go
@@ -0,0 +1,24 @@
+package node
+
+type node struct {
+	ip   string
+	name string
+	tags []string
+}
+
+func NewNode(ip string, name string, tags []string) *node {
+    n := node{ip: ip, name: name, tags: tags}
+    return &n
+}
+
+func (n node) GetIp() string {
+	return n.ip
+}
+
+func (n node) GetName() string {
+	return n.name
+}
+
+func (n node) GetTags() []string {
+	return n.tags
+}
